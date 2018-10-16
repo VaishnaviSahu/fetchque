8c745e9146f4e944bb41516114ff8eb1a60f0d16
VaishnaviSahu <vaishnavi.sahu@cgi.com>
2018-10-16 09:31:00 +0530

    score of all

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,45 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-//import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-//import { HomeComponentComponent } from './home-component/home-component.component';
-//import { HeaderComponentComponent } from './home-component/header-component/header-component.component';
 import { QuizComponentComponent } from './quiz/quiz.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-//import {MatDividerModule} from '@angular/material/divider';
-
-//import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatButtonModule} from '@angular/material/button';
 import {MatRadioModule} from '@angular/material/radio';
 import { HomeComponent } from './home/home.component';
 import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    //HomeComponentComponent,
-    //HeaderComponentComponent,
     QuizComponentComponent,
     HomeComponent,
   
   ],
   imports: [
     BrowserModule,
-   // AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    //MatToolbarModule,
     MatGridListModule,
-    //MatDividerModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatRadioModule,
     MatInputModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -2,25 +2,44 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 
- 
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
   styleUrls: ['./quiz.component.css']
 })
+
 export class QuizComponentComponent implements OnInit {
 
   res: any = [ ];
   questions = [ ];
+  players = [];
+  playRes: any[];
   counter:number = 10;
   i:number=0;
+  name: string;
   score:number=0;
   questionCounter = 0;
   shouldDisplayQuestions = false;
   currentQuestion : any;
+
+
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    console.log('called showPlayer');
+  this.http.get('http://localhost:3000/players').subscribe((playres: any) => {
+  this.players = playres;
+  this.score=playres.score;
+  for(let i = 0; i < this.tiles.length;i++){
+    this.tiles[i].text = this.players[i].name +" : "+ this.players[i].score;
+  }
+});
     
   }
 
@@ -57,4 +76,20 @@ resetTimer(){
   //this.score+=this.counter*2;
   this.counter=10;
 }
+/*
+showPlayer(){
+  console.log('called showPlayer');
+  this.http.get('http://localhost:3000/players').subscribe((playres: any) => {
+  this.players = playres;
+  this.score=playres.score;
+});
+}*/
+
+tiles: Tile[] = [
+  {text: 'One', cols: 1, rows: 1, color: 'lightblue'},
+  {text: 'Two', cols: 1, rows: 1, color: 'lightgreen'},
+  {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+  {text: 'Four', cols: 1, rows: 1, color: '#DDBDF1'},
+];
+
 }
