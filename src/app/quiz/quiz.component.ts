299ab03ade6f3fc67fadaf6c1acdafcba714442b
VaishnaviSahu <vaishnavi.sahu@cgi.com>
2018-10-18 16:52:24 +0530

    real time score

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -28,6 +28,10 @@ export class QuizComponentComponent implements OnInit {
   shouldDisplayQuestions = false;
   currentQuestion : any;
 
+//  quesCount=0;
+
+  gameOver = false;
+ 
 
   constructor(private http: HttpClient) { }
 
@@ -35,7 +39,7 @@ export class QuizComponentComponent implements OnInit {
     console.log('called showPlayer');
   this.http.get('http://localhost:3000/players').subscribe((playres: any) => {
   this.players = playres;
-  this.score=playres.score;
+  //this.score=playres.score;
   for(let i = 0; i < this.tiles.length;i++){
     this.tiles[i].text = this.players[i].name +" : "+ this.players[i].score;
   }
@@ -61,19 +65,26 @@ export class QuizComponentComponent implements OnInit {
     this.counter--;
     if (this.counter <= 0) {
       this.nextQuestion();
-      this.resetTimer();
+      //this.resetTimer();
+      if(this.questionCounter==7)
+      {
+        clearInterval(intervalMain);
+        this.gameOver=true;
+      }
     }
   }, 1000);
 }
 
 nextQuestion(){
+  this.resetTimer();
   this.questionCounter++;
   this.currentQuestion = this.questions[this.questionCounter];
 }
 
 resetTimer(){
   this.i++;
-  //this.score+=this.counter*2;
+  //this.quesCount++;
+  this.score+=this.counter*2;
   this.counter=10;
 }
 /*
